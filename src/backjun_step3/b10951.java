b2e4744550007df7c510dc46dcf10056adca5c89
김용선 <metalgos2@gmail.com>
2023-10-17 11:28:54 +0900

    단계별로 풀어보기 3단계 완료

---
+++ src/backjun_step3/b10951.java
@@ -0,0 +1,20 @@
+package backjun_step3;
+
+import java.util.Scanner;
+
+public class b10951 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        while (sc.hasNextInt()) {
+
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            System.out.println(a + b);
+
+        }
+    }
+}
--- src/backjun_step3/b2439.java
+++ src/backjun_step3/b2439.java
@@ -2,15 +2,19 @@ package backjun_step3;
 
 import java.util.Scanner;
 
-public class b2438 {
+public class b2439 {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in   );
         int step = sc.nextInt();
 
         for (int i =1; i<=step; i++){
+            for (int j =i; j<step; j++){
+                System.out.print(" ");
+            }
             for (int j = step-i; j<step; j++){
                 System.out.print("*");
             }
+
             if(i!=step){
                 System.out.println();
             }
@@ -18,3 +22,4 @@ public class b2438 {
         }
     }
 }
+
