2037e224042e339b2be2a25871d3ae457ebfa969
김용선 <metalgos2@gmail.com>
2023-10-27 10:07:17 +0900

    solved.ac class2 b1654 이진 탐색트리로 풀어야 시간초과없음

--- src/solved_class2/b1654.java
+++ src/solved_class2/b1654.java
@@ -5,57 +5,53 @@ import java.util.Scanner;
 public class b1654 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int fstep = sc.nextInt();
-        long lstep = sc.nextInt();
+        int K = sc.nextInt();
+        long N = sc.nextInt();
 
-        int arr[] = new int[fstep];
 
-        int max = 0;
-
-        long count = 0;
         long result = 0;
+        long max = 0;
 
-        for (int i = 0; i < fstep; i++) {
-            int n = sc.nextInt();
-            arr[i] = n;
-
-
-            if (max < arr[i]) {
-                max = arr[i];
-
-            }
-
+        long[] arr = new long[K];
+        for (int i = 0; i < K; i++) {
+            arr[i] = sc.nextInt();
+            max = Math.max(max, arr[i]);
         }
-        int k = 1;
-        if(max!=0){
-            k=max/2;
+
+        long k = 1;
+        if (max != 0) {
+            k = max / 2;
         }
 
 
-        lp1:
+        long half = 0;
+        long min = 1;
 
+        while (min <= max) {
+            half = (min + max) / 2;
+            long count = 0;
 
-        for (; k < max;) {
-            long temp = result;
-
-            for (int i = 0; i < arr.length; i++) {
-
-                count += arr[i] / k;
-                result = k;
-
+            for (long num : arr) {
+                count += num / half;
             }
 
-
-            if (count < lstep) {
-
-                System.out.println(temp);
-
-                break lp1;
+            // 원하는 랜선 갯수 보다 잘라진 랜선 수가 적을경우
+            // 하나의 랜선 길이가 길어서 길이를 더 짧게 만들어야 함
+            // half보다 아래의 수.
+            if (count < N) {
+                max = half - 1;
             }
-            count = 0;
+            // 원하는 랜선 갯수 보다 잘라진 랜선 수가 많을경우
+            // 하나의 랜선 길이가 짧아서 더 길게 만들어야 함
+            // half보다 위의 수에 있음.
+            else {
+                min = half + 1;
+            }
         }
 
-
+        System.out.println((max + min) / 2);
     }
 
+
 }
+
