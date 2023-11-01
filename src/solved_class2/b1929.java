c0a4bfb390ee311ddd24f7ac13f09d6e5f5758c3
김용선 <metalgos2@gmail.com>
2023-11-01 18:54:32 +0900

    solved.ac class2

---
+++ src/solved_class2/b1929.java
@@ -0,0 +1,18 @@
+package solved_class2;
+
+import java.util.Scanner;
+
+public class b1929 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+
+
+
+
+
+
+
+    }
+}
