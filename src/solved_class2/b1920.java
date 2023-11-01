b1cc2e818e83cc0224bb998ecaa5875fe00fbe17
김용선 <metalgos2@gmail.com>
2023-11-01 18:44:53 +0900

    solved.ac class2 1920 이진탐색으로 수정

--- src/solved_class2/b1920.java
+++ src/solved_class2/b1920.java
@@ -13,28 +13,50 @@ public class b1920 {
 
 
         int step = sc.nextInt();
-        //nt[] first = new int[step];
+
         for (int i = 0; i < step; i++) {
-            //irst[i] = sc.nextInt();
             flist.add(sc.nextInt());
-
-
         }
         Collections.sort(flist);  // 첫번째 리스트 소트
-
-
         int lstep = sc.nextInt();
-        //int[] last = new int[lstep];
+
         List<Integer> lalist = new ArrayList<>();
 
         for (int i = 0; i < lstep; i++) {
-            //ast[i] = sc.nextInt();
             lalist.add(sc.nextInt());
-
+        }
+        for (int i = 0; i < lstep; i++) {
+            int result = binarysearch(flist, lalist.get(i));
+            System.out.println(result);
 
         }
+
+
+    }
+
+    static int binarysearch(List<Integer> flist, int key) {
+
         int start = 0;
         int end = flist.size() - 1;
+
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (key == flist.get(mid)) {
+                return 1;
+
+            } else if (key > flist.get(mid)) {
+                start = mid + 1;
+
+            } else {
+                end = mid - 1;
+            }
+
+            //System.out.print(start + " " + end);
+        }
+
+
+        return 0;
     }
 
 
