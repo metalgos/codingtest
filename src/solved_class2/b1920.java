6d2749301ac847d0af45986768eea224a0a6c87d
김용선 <metalgos2@gmail.com>
2023-11-01 13:57:22 +0900

    solved.ac class2 1920 수정중

--- src/solved_class2/b1920.java
+++ src/solved_class2/b1920.java
@@ -1,51 +1,41 @@
 package solved_class2;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class b1920 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        List<Integer> flist = new ArrayList<>();
+
+
         int step = sc.nextInt();
-        int[] first = new int[step];
+        //nt[] first = new int[step];
         for (int i = 0; i < step; i++) {
-            first[i] = sc.nextInt();
+            //irst[i] = sc.nextInt();
+            flist.add(sc.nextInt());
 
 
         }
+        Collections.sort(flist);  // 첫번째 리스트 소트
 
 
         int lstep = sc.nextInt();
-        int[] last = new int[lstep];
+        //int[] last = new int[lstep];
+        List<Integer> lalist = new ArrayList<>();
 
         for (int i = 0; i < lstep; i++) {
-            last[i] = sc.nextInt();
+            //ast[i] = sc.nextInt();
+            lalist.add(sc.nextInt());
 
 
         }
-
-        boolean count = true;
-
-        //기존 정렬 복잡도 높기 떄문에 다른정렬 방법 찾아서 수정해야함
-
-        for (int i = 0; i < step; i++) {
-            for (int k = 0; k < lstep; k++) {
-                if(first[k]==last[i]){
-                    System.out.println(1);
-
-
-                    count = false;
-                    break;
-                }
-
-            }
-            if(count){
-                System.out.println(0);
-
-            }
-            count = true;
-        }
-
-
+        int start = 0;
+        int end = flist.size() - 1;
     }
+
+
 }
