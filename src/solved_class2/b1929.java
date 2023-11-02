de2b590e208075d3859afd6fd59cdaaa66e47efd
김용선 <metalgos2@gmail.com>
2023-11-02 11:10:21 +0900

    solved.ac class2

--- src/solved_class2/b1929.java
+++ src/solved_class2/b1929.java
@@ -1,11 +1,15 @@
 package solved_class2;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class b1929 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
+        int m = sc.nextInt();
 
 
 
@@ -13,6 +17,27 @@ public class b1929 {
 
 
 
+
+        /*
+        for (int i = n; i <= m; i++) {
+            int isprime = 0;
+            for (int k = 2; k < i; k++) {
+
+
+                if (i % k == 0) {
+                    isprime = 0;
+                    break;
+                } else {
+                    isprime = 1;
+                }
+            }
+            if (isprime == 1) {
+                System.out.println(i);
+            }
+
+
+        }
+*/
 
     }
 }
