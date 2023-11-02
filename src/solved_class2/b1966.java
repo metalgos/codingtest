2a4060143f56892abb3921ea45409dc746ee29c2
김용선 <metalgos2@gmail.com>
2023-11-02 15:00:26 +0900

    solved.ac class2

---
+++ src/solved_class2/b1966.java
@@ -0,0 +1,18 @@
+package solved_class2;
+
+import java.util.Scanner;
+
+public class b1966 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n= sc.nextInt();
+
+        int m = sc.nextInt();
+
+
+
+
+
+    }
+}
